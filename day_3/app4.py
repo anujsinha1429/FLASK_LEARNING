e75aef29bc761913d08a96cf66778366d62ab409
Anuj Sinha <229733784+anujsinha1429@users.noreply.github.com>
2026-02-14 09:46:33 +0530

    Day 2 flask learning

---
+++ day_3/app4.py
@@ -0,0 +1,17 @@
+# jinja2 template example with conditional and loops
+
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def student_profile ():
+    return render_template(
+        "profile.html",
+        name="Arun",
+        is_topper = True ,
+        subjects = ["maths","science","english"]
+                           
+    )
+
+
